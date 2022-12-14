8a91642d32c29ad349a7fb611c47a9bf10d331b4
HlisTilen <tilen.hlis@gmail.com>
2022-12-14 11:55:26 +0100

    fix: serachinput

--- CLMGui/src/components/Search/SearchInput/SearchInput.tsx
+++ CLMGui/src/components/Search/SearchInput/SearchInput.tsx
@@ -1,43 +1,12 @@
 import * as React from "react";
-import { useState } from "react";
 import TextField from "@mui/material/TextField";
-import ISearch from "../../../types/search/searchPost.type";
-import SearchDataService from "../../../services/search.service";
-import ISearchResponse from "../../../types/search/searchResponse.type";
 
 const textFieldStyle = {
   width: "100%",
 };
 
 const SearchInput = () => {
-  const [searchRes, setSearchRes] = useState<ISearchResponse | null>(null);
-  const [wordEntered, setWordEntered] = useState<string>("");
-
-  const handleSearch = async (
-    target: React.ChangeEvent<HTMLInputElement>
-  ): Promise<void> => {
-    const searchWord = target.target.value;
-    setWordEntered(searchWord);
-
-    const data: ISearch = {
-      headers: {
-        //TODO: get from auth
-        userToken: "user@clm.com", //TODO: user must be under allowed users (in database)
-        tenantUniqueName: searchWord,
-      },
-      body: { sortBy: "name" }, //TODO: check...
-    };
-
-    try {
-      const searchResponse = await SearchDataService.create(data);
-      setSearchRes(searchResponse);
-    } catch (error) {
-      //dont know what all of the exceptions from spring are...
-      console.log(error);
-    }
-
-    console.log(searchRes);
-  };
+ 
 
   return (
     <TextField
@@ -45,8 +14,6 @@ const SearchInput = () => {
       label="Iskanje"
       variant="outlined"
       style={textFieldStyle}
-      value={wordEntered}
-      onChange={handleSearch}
       placeholder="Curently only tenant unique name is supported (must be in database)..."
     />
   );
